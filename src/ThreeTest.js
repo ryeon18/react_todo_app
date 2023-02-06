567b437e0f44e78fae9f8ae8aa8edf8cae2f762f
sy Park <sy.park@weballin.com>
2023-02-06 15:33:15 +0900

    three js 추가

--- src/ThreeTest.js
+++ src/ThreeTest.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import {Canvas} from '@react-three/fiber';
+import {OrbitControls} from '@react-three/drei';
 
 const ThreeTest = () => {
   return (
     <div>
-      <p>ThreeTest!</p>
+      <Canvas>
+        <OrbitControls autoRotate={true} />
+        <mesh>
+          <ambientLight intensity={1} />
+          <directionalLight position={[-1, 0, 1]} intensity={0.5} />
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial attach="material" color={0xa3b18a} />
+        </mesh>
+      </Canvas>
     </div>
   );
 };
