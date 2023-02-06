9d0ee431c413e3ea126bfe6249173b833edd7105
sy Park <sy.park@weballin.com>
2023-02-07 08:28:13 +0900

    threejs cylinderGeometry rotate loop

--- src/pages/Rotate.js
+++ src/pages/Rotate.js
@@ -1,51 +1,27 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useRef} from 'react';
 import {Canvas, useFrame} from '@react-three/fiber';
-import {OrbitControls, Stars} from '@react-three/drei';
-import gsap from 'gsap';
-
-import {useNavigate} from 'react-router-dom';
 
 const CoinMesh = () => {
   const mesh = useRef(null);
-
-  useEffect(() => {
-    gsap.to(mesh.current.position, {
-      duration: 1,
-      y: 2,
-    });
-  }, [mesh]);
-
-  useFrame((state, delta) => {
-    gsap.to(mesh.current.rotation, {
-      y: state.clock.elapsedTime,
-    });
-  });
+  useFrame(() => (mesh.current.rotation.y = mesh.current.rotation.z += 0.1));
 
   return (
-    <mesh ref={mesh} scale={0.2} position={[Math.random() * 5 - 2.5, 0, Math.random() * 5 - 2.5]}>
+    <mesh ref={mesh} scale={0.3}>
       <ambientLight intensity={1} />
-      <directionalLight position={[-1, 0, 1]} intensity={0.5} />
-      <cylinderGeometry color="red" arg={[1, 1, 0.3, 50]} />
-      <meshLambertMaterial attach="material" color="yellow" />
+      <cylinderGeometry color="red" args={[1, 1, 0.3, 50]} />
+      <meshLambertMaterial attach="material" color="green" />
     </mesh>
   );
 };
 
-const Rotate = () => {
-  const navigation = useNavigate();
-  const goToBack = e => {
-    navigation(-1);
-  };
-
+const Meshes = () => {
   return (
-    <div className="rotate-test">
-      <Canvas onClic={e => goToBack(e)}>
-        <Stars />
-        <OrbitControls autoRotate={true} />
+    <>
+      <Canvas>
         <CoinMesh />
       </Canvas>
-    </div>
+    </>
   );
 };
 
-export default Rotate;
+export default Meshes;
